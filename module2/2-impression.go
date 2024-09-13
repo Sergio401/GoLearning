cb1226be2f2389b42a31bfd880651ce2c21b9ed4
Sergio401 <sergioleonardo.gonzalez@gmail.com>
2024-09-12 22:57:31 -0500

    Add impressions module

--- module2/2-impression.go
+++ module2/2-impression.go
@@ -7,17 +7,17 @@ func main() {
     num := 123
     boolean := true
 
-    fmt.Printf("String: %s\n", str)  // %s para strings
-    fmt.Printf("Entero: %d\n", num)  // %d para enteros
-    fmt.Printf("Booleano: %t\n", boolean) // %t para booleanos
-    fmt.Printf("Hexadecimal: %x\n", num) // %x para enteros en hexadecimal
-    fmt.Printf("Tipo: %T\n", num)    // %T para mostrar el tipo de variable
+    fmt.Printf("String: %s\n", str)  // %s to strings
+    fmt.Printf("Int: %d\n", num)  // %d to integers
+    fmt.Printf("Boolean: %t\n", boolean) // %t to booleans
+    fmt.Printf("Hexadecimal: %x\n", num) // %x to integers in hexadecimal
+    fmt.Printf("Type: %T\n", num)    // %T to show the type of a variable
 
 	// Type conversions
 
 	numInString := float32(num)
-	fmt.Printf("Valor: %v\n", numInString)
-	fmt.Printf("Tipo: %T\n", numInString)
+	fmt.Printf("Value: %v\n", numInString)
+	fmt.Printf("Type: %T\n", numInString)
 
 	impedance := complex(2, 3)
 	fmt.Println(impedance)
